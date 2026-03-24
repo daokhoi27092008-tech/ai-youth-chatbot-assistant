85c3e43994be1b4d3af4cffb3313c1f6ed59d4b8
Khoi <daokhoi27092008@gmail.com>
2026-03-24 22:37:47 +0700

    fix deploy

--- app.py
+++ app.py
@@ -82,5 +82,8 @@ def reset():
     session.clear()
     return jsonify({"status": "ok"})
 
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
